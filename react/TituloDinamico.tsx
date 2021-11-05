866adc821eb221571e55fdfa38397ef96c830c97
Newton Figueiredo <new.figueiredo@gmail.com>
2021-11-05 12:01:23 -0300

    adequando codigo

--- react/TituloDinamico.tsx
+++ react/TituloDinamico.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
 
-const titulodinamico: StorefrontFunctionComponent = () => {
+
+
+type Propstitulodinamico ={
+  titulo:string
+}
+
+const titulodinamico: StorefrontFunctionComponent<Propstitulodinamico> = ( {titulo}:Propstitulodinamico ) => {
+
+      console.log(titulo);
+
       return(
         <div>
           <h1>Teste</h1>
+          <h1>{titulo}</h1>
         </div>
       )
+}
+
+titulodinamico.schema = {
+  title:"Conteudo do titulo",
+  description:"descricao",
+  type:"object",
+  properties:{
+    title:"Conteudo do titulo",
+    description:"descricao",
+    type:"string",
+    default:null
+  }
 }
 
 export default titulodinamico
